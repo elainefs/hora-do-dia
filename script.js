c1a8c556e0a0c048307844ca3bcc2752dac45bf5
Elaine Ferreira <elaineferreira.dev@gmail.com>
2023-09-26 14:45:26 -0300

    fix: image url on script

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ function carregar() {
   msg.innerHTML = `Agora são ${hora}:${min}:${sec}`
 
   if (hora >= 0 && hora < 12) {
-    img.src = './src/manha.jpg'
+    img.src = '/src/manha.jpg'
     document.body.style.background = '#e2cd9f'
   } else if (hora >= 12 && hora < 18) {
-    img.src = './src/tarde.jpg'
+    img.src = '/src/tarde.jpg'
     document.body.style.background = '#b9846f'
   } else {
-    img.src = './src/noite.jpg'
+    img.src = '/src/noite.jpg'
     document.body.style.background = '#515154'
   }
 }
