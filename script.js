3b091c6601a4d419b43c310678555a04c171bcf9
Elaine Ferreira <elaineferreira.dev@gmail.com>
2023-09-26 14:39:05 -0300

    feat: add main files

---
+++ script.js
@@ -0,0 +1,25 @@
+function carregar() {
+  var msg = window.document.getElementById('msg')
+  var img = window.document.getElementById('imagem')
+  
+  var data = new Date()
+  var hora = data.getHours()
+  var min = data.getMinutes()
+  var sec = data.getSeconds()
+
+  msg.innerHTML = `Agora são ${hora}:${min}:${sec}`
+
+  if (hora >= 0 && hora < 12) {
+    img.src = './src/manha.jpg'
+    document.body.style.background = '#e2cd9f'
+  } else if (hora >= 12 && hora < 18) {
+    img.src = './src/tarde.jpg'
+    document.body.style.background = '#b9846f'
+  } else {
+    img.src = './src/noite.jpg'
+    document.body.style.background = '#515154'
+  }
+}
+
+setInterval(carregar)
+
